199f472bb80e4c6214e04c9a09d0e2019a5d8a04
Variasko <121598389+Variasko@users.noreply.github.com>
2025-05-21 15:58:31 +0300

    library

---
+++ Программный модуль/Library/ClassLibrary/MaterialCalculatorLib.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ClassLibrary
+{
+    public class MaterialCalculatorLib
+    {
+        private readonly FloorMasterEntities _db = new FloorMasterEntities();
+
+        public int CalculateMaterial(
+            int productTypeId,
+            int materialTypeId,
+            int productAmount,
+            double param1,
+            double param2
+        )
+        {
+            try
+            {
+                // Валидация входных параметров
+                if (productAmount <= 0 || param1 <= 0 || param2 <= 0)
+                    return -1;
+
+                // Проверка существования типа продукции
+                var productType = _db.ProductType
+                    .AsNoTracking()
+                    .FirstOrDefault(pt => pt.Id == productTypeId);
+
+                if (productType == null || productType.ProductTypeCoefficient <= 0)
+                    return -1;
+
+                // Проверка существования типа материала
+                var materialType = _db.MaterialType
+                    .AsNoTracking()
+                    .FirstOrDefault(mt => mt.Id == materialTypeId);
+
+                if (materialType == null || materialType.PercentageDefectiveMaterial < 0)
+                    return -1;
+
+                // Расчет материала на единицу продукции
+                double materialPerUnit = param1 * param2 * (double)productType.ProductTypeCoefficient;
+
+                // Учет брака
+                double totalMaterial = materialPerUnit * productAmount;
+                totalMaterial *= (1 + (double)materialType.PercentageDefectiveMaterial);
+
+                // Округление вверх до целого
+                return (int)Math.Ceiling(totalMaterial);
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ Программный модуль/Library/Demonstration/Program.cs
@@ -0,0 +1,32 @@
+﻿using ClassLibrary;
+
+namespace Demonstration
+{
+    class App
+    {
+        public static void Main()
+        {
+            MaterialCalculatorLib _mcl = new MaterialCalculatorLib();
+
+            Console.Write("Введите тип продукции: ");
+            int productType = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите тип материала: ");
+            int materialType = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите количество желаемой продукции: ");
+            int productAmount = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите параметр 1: ");
+            double param1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите параметр 2: ");
+            double param2 = double.Parse(Console.ReadLine());
+
+            int result = _mcl.CalculateMaterial(productType, materialType, productAmount, param1, param2);
+
+            Console.WriteLine($"На {productAmount} ед. продукции " +
+                              $"потребуется {result} ед. материала.");
+        }
+    }
+}
